f8bbb6f6e9595507b503f77a8e53a911f7562884
MatheusVict <matheusvictorhenrique@gmail.com>
2024-10-19 10:48:55 -0300

    feat: add binary search method

---
+++ src/main/java/io/matheusvictor/BinarySearch.java
@@ -0,0 +1,33 @@
+package io.matheusvictor;
+
+import java.util.Scanner;
+
+/**
+ * O(log n)
+ */
+public class BinarySearch {
+    public static BinarySearchResult binarySearch(int[] vector, int choice) {
+        int count = 0;
+        boolean found = false;
+        int start = 0;
+        int end = vector.length - 1;
+        int middle = 0;
+
+        while (start <= end) {
+            count++;
+            middle = (start + end) / 2;
+
+            if (vector[middle] == choice) {
+                found = true;
+                break;
+            } else if (vector[middle] < choice) {
+                start = middle + 1;
+            } else {
+                end = middle - 1;
+            }
+        }
+
+        return new BinarySearchResult(middle, vector[middle], vector.length, count, found);
+    }
+
+}
---
+++ src/main/java/io/matheusvictor/BinarySearchResult.java
@@ -0,0 +1,4 @@
+package io.matheusvictor;
+
+public record BinarySearchResult(Integer index, Integer value, Integer size, Integer operations, Boolean found) {
+}
